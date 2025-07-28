ec0a41afc55b0604c6ad7859e9542f58b5af8cdb
Michael Bryant <e.kazuma@gmail.com>
2025-07-28 12:18:40 -0400

    Added flight controls

--- src/UI/components/Player/Player.tsx
+++ src/UI/components/Player/Player.tsx
@@ -1,32 +1,52 @@
 import { PerspectiveCamera, useKeyboardControls } from "@react-three/drei"
 import { useFrame } from "@react-three/fiber"
-import { useRef } from "react"
-import { Euler, Quaternion, Vector3 } from "three"
+import { Quaternion, Vector2, Vector3 } from "three"
+import { AppViews, useAppStore } from "../../../stores/AppStore"
 
 
 export const Player = () => {
-  const dir = useRef<Vector3>(new Vector3(0, 0, -1))
+  const view = useAppStore(state => state.view)
   const [, getKeys] = useKeyboardControls()
+  const rotationSpeed = new Vector2(0.25, 0.25)
   useFrame((state, delta) => {
+    if (view != AppViews.game) return
+
     const { up, down, left, right } = getKeys()
     const upVal = up ? 1 : 0
     const downVal = down ? -1 : 0
     const leftVal = left ? 1 : 0
     const rightVal = right ? -1 : 0
-    dir.current.applyQuaternion(new Quaternion().setFromEuler(new Euler(
-      (upVal + downVal) * 0.1 * delta,
-      (leftVal + rightVal) * 0.1 * delta,
-      0
-    )))
-    const step = dir.current.clone().normalize().multiplyScalar(0.1 * delta)
+    const currentDir = new Vector3()
+    const currentQuaternion = new Quaternion()
+    const rollVector = new Vector3(0, 0, 1)
+    const pitchVector = new Vector3(1, 0, 0)
+    state.camera.getWorldQuaternion(currentQuaternion)
+
+    // Calculate and apply rotations
+    const rollQuaternion = new Quaternion().setFromAxisAngle(
+      rollVector,
+      (leftVal + rightVal) * rotationSpeed.y * delta,
+    )
+    const pitchQuaternion = new Quaternion().setFromAxisAngle(
+      pitchVector,
+      (upVal + downVal) * rotationSpeed.x * delta,
+    )
+    state.camera.quaternion
+      .multiply(rollQuaternion)
+      .multiply(pitchQuaternion)
+
+    
+    // Calculate and apply position
+    state.camera.getWorldDirection(currentDir)
+    const step = currentDir.clone().normalize().multiplyScalar(0.1 * delta)
     state.camera.position.add(step)
-    state.camera.lookAt(state.camera.position.clone().add(step))
   })
 
   return (
     <group>
       <PerspectiveCamera
         makeDefault
+        near={0.001}
       >
       </PerspectiveCamera>
     </group>
